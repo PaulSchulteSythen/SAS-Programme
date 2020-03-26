9f870c943d79c75996399259afd264239b636c89
PaulSchulteSythen <paulsyhten@gmail.com>
2020-03-26 15:00:35 +0100

    test

--- AB2.4&5_malewitschMitTextur/Aufgabe4.java
+++ AB2.4&5_malewitschMitTextur/Aufgabe4.java
@@ -1,3 +1,4 @@
+
 /**
  * Zeichnung zweier Vierecke
  * 
@@ -6,7 +7,7 @@
  */
 import sas.*;
 import java.awt.Color;
-public class Malewitsch2
+public class Aufgabe4
 {
     View view;
     Rectangle hintergrund;
@@ -15,16 +16,13 @@ public class Malewitsch2
     /**
      * Konstruktor für Objekte der Klasse Malewitsch
      */
-    public Malewitsch2()
+    public Aufgabe4()
     {
-        view = new View(330, 470, "Zwei Vierecke");
+        view = new View("Schwarzes und rotes Viereck");
         hintergrund = new Rectangle(2, 2, 596, 396, "texturMalewitsch.png");
-        hintergrund.move(-120);
-        hintergrund.turn(90);
-        hintergrund.move(50);
-        s_rectangle = new Rectangle(50, 30, 200, 180, Color.BLACK);
+        s_rectangle = new Rectangle(50, 110, 200, 180, Color.BLACK);
         s_rectangle.turn(-10);
-        r_rectangle = new Rectangle(60, 270, 150, 150, new Color(140,50,47));
+        r_rectangle = new Rectangle(350, 125, 150, 150, new Color(140,50,47));
         r_rectangle.turn(20);
         
     }
--- AB2.4&5_malewitschMitTextur/Aufgabe5.java
+++ AB2.4&5_malewitschMitTextur/Aufgabe5.java
@@ -1,4 +1,3 @@
-
 /**
  * Zeichnung zweier Vierecke
  * 
@@ -7,7 +6,7 @@
  */
 import sas.*;
 import java.awt.Color;
-public class Malewitsch
+public class Aufgabe5
 {
     View view;
     Rectangle hintergrund;
@@ -16,13 +15,16 @@ public class Malewitsch
     /**
      * Konstruktor für Objekte der Klasse Malewitsch
      */
-    public Malewitsch()
+    public Aufgabe5()
     {
-        view = new View("Schwarzes und rotes Viereck");
+        view = new View(330, 470, "Zwei Vierecke");
         hintergrund = new Rectangle(2, 2, 596, 396, "texturMalewitsch.png");
-        s_rectangle = new Rectangle(50, 110, 200, 180, Color.BLACK);
+        hintergrund.move(-120);
+        hintergrund.turn(90);
+        hintergrund.move(50);
+        s_rectangle = new Rectangle(50, 30, 200, 180, Color.BLACK);
         s_rectangle.turn(-10);
-        r_rectangle = new Rectangle(350, 125, 150, 150, new Color(140,50,47));
+        r_rectangle = new Rectangle(60, 270, 150, 150, new Color(140,50,47));
         r_rectangle.turn(20);
         
     }
